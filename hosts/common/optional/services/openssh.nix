04b76105ad4e571ffb50bbd5a27b707ad586cce7
Kristopher Bredemeier <k.bredemeier@proton.me>
2025-10-19 12:16:37 +0200

    config: Opens openssh up

--- hosts/common/optional/services/openssh.nix
+++ hosts/common/optional/services/openssh.nix
@@ -16,8 +16,8 @@ in
 
     settings = {
       # Harden
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
+      # PasswordAuthentication = false;
+      # PermitRootLogin = "no";
       # Automatically remove stale sockets
       StreamLocalBindUnlink = "yes";
       # Allow forwarding ports to everywhere
