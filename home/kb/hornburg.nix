efadb33bbf3ce42500c1df89f89bb712e4b41d9a
Kristopher Bredemeier <k.bredemeier@proton.me>
2025-11-02 21:20:05 +0100

    chore: Relocates common home modules

--- home/kb/hornburg.nix
+++ home/kb/hornburg.nix
@@ -2,24 +2,20 @@
 # the corresponding home-level files are housed in each user's home-level config directory. This allows you to customize
 # user-specific, home-manager configurations on a per user basis. The `home/common/optional/foo` configs, along with
 # `home/common/core` allow you to import the specific home-manager configs you want for each host
-{ ... }:
+{ lib, ... }:
 {
-  imports = [
-    #
-    # ========== Required Configs ==========
-    #
-    common/core
-
-    #
-    # ========== Host-specific Optional Configs ==========
-    #
-    # FIXME(starter): add or remove any optional config directories or files here
-    common/optional/browsers
-    common/optional/desktops
-    common/optional/comms
-    common/optional/media
-
-    common/optional/sops.nix
-  ];
-
+  imports = (
+    map lib.custom.relativeToRoot (
+      [
+        "home/common/core"
+      ]
+      ++ (map (f: "home/common/optional/${f}") [
+        "browsers"
+        "desktops"
+        "comms"
+        "media"
+        "sops.nix"
+      ])
+    )
+  );
 }
